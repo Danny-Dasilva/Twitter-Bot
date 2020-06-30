eb5756d830b09b143b20254b0ce238e9f3b0573b
Danny Dasilva <yahchayildasilva@gmail.com>
2020-06-30 04:09:43 -0400

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,9 @@
+
+wget https://github.com/mozilla/geckodriver/releases/download/v0.26.0/geckodriver-v0.26.0-linux64.tar.gz
+# or new release if it exists
+
+tar -xvzf geckodriver-v0.26.0-linux64.tar.gz 
+
+chmod +x geckodriver
+
+sudo mv geckodriver /usr/local/bin/
